5996358ec301bbb7bc75e72bd973274c17d7d945
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2024-09-02 10:48:54 +0530

    Insertion sort implement

--- SELF1/DAY 1/Sort.java
+++ SELF1/DAY 1/Sort.java
@@ -17,27 +17,35 @@ class Sort
         }
         }   
     }
-      public static void selectionSort(int arr[])
-
-{
-    for(int i=0;i<arr.length-1;i++)
-    {
-        int minpos=i;
-        for(int j=i+1;j<arr.length-1;j++)
-       
-        {
-            if(arr[minpos]>arr[j])
-            {
-                minpos=j;
-
+   public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minpos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minpos] > arr[j]) {
+                    minpos = j;
+                }
             }
+            int temp = arr[i];
+            arr[i] = arr[minpos];
+            arr[minpos] = temp;
         }
-        int temp=arr[i];
-        int arr[i]=arr[minpos];
-        arr[minpos]=temp;
     }
-}
-    
+
+
+    public static void insertionSort(int arr[])
+    {
+        for(int i=1<arr.length;i++)
+        {
+            int cuu=arr[i];
+            int key=i-1;
+            while(key>=0 && arr[key]>cuu)
+            {
+                arr[key+1]=arr[key];
+                 key--;
+            }
+            arr[key+1]=cuu;
+        }
+    }
     public static void printarr(int arr[])
     {
         for(int i:arr)
@@ -50,7 +58,8 @@ class Sort
     {
         int[] arr = {10, 5, 3, 1, 2, 4, 8, 7, 6, 9};     
         // buublesort(arr);   
-        selectionSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
 
         printarr(arr);
            
