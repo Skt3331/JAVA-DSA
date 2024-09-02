50aa64ffe97c4aac1c8e7131d64b30a8a2298d39
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2024-09-02 09:48:01 +0530

    implement selection sort

--- SELF1/DAY 1/Sort.java
+++ SELF1/DAY 1/Sort.java
@@ -6,18 +6,39 @@ class Sort
         {
         for(int j=0;j<arr.length-i-1;j++)
         {
-            if(arr[j]>arr[i])
+            if(arr[j]>arr[j+1])
             {
                 //swap
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=arr[i];
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
 
             }
         }
         }   
     }
-    public void printarr(int arr[])
+      public static void selectionSort(int arr[])
+
+{
+    for(int i=0;i<arr.length-1;i++)
+    {
+        int minpos=i;
+        for(int j=i+1;j<arr.length-1;j++)
+       
+        {
+            if(arr[minpos]>arr[j])
+            {
+                minpos=j;
+
+            }
+        }
+        int temp=arr[i];
+        int arr[i]=arr[minpos];
+        arr[minpos]=temp;
+    }
+}
+    
+    public static void printarr(int arr[])
     {
         for(int i:arr)
         {
@@ -28,7 +49,9 @@ class Sort
     public static void main(String[] args)
     {
         int[] arr = {10, 5, 3, 1, 2, 4, 8, 7, 6, 9};     
-        buublesort(arr);
+        // buublesort(arr);   
+        selectionSort(arr);
+
         printarr(arr);
            
 }
